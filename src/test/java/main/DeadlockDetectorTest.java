e6fa206da2feea7003e7da41183608263744b706
Josh DeWitt <jdewitt@unomaha.edu>
2013-05-29 22:52:54 -0500

    Refactored test to be easier to add to.

--- src/test/java/main/DeadlockDetectorTest.java
+++ src/test/java/main/DeadlockDetectorTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -11,13 +13,34 @@ public class DeadlockDetectorTest {
 
     @DataProvider(name = "testCases")
     public Object[][] createTestCases() {
-        return new Object[][]{
-                {null, "1 1 2 1 1 2 1", "Simulation 1\n" +
-                        "All processes successfully terminated.\n" +
-                        "Process 1: run time = 2, ended at 1\n"},
-                {null, "2 2 2 1 1 1 2 2 1 2 1 1", "Simulation 1\n" +
-                        "Deadlock detected at time 2 involving...\n" +
-                        "\tprocesses: 1 2\n\tresources: 2 1"}};
+        List<String> inputs = new ArrayList<String>();
+        List<String> outputs = new ArrayList<String>();
+
+        inputs.add("1 2 5 1 1 1 2 3 10 2 1 2 2");
+        outputs.add("All processes successfully terminated.\n" +
+                "Process 1: run time = 14, ended at 13");
+        inputs.add("3 3 5 1 1 1 2 3 2 2 1 2 2 5 1 1 1 2 3 2 2 1 2 2 4 1 3 3 5 2 3 3 2");
+        outputs.add("All processes successfully terminated.\n" +
+                "Process 1: run time = 6, ended at 12\n" +
+                "Process 2: run time = 7, ended at 21\n" +
+                "Process 3: run time = 9, ended at 19");
+        inputs.add("2 2 2 1 1 1 2 2 1 2 1 1");
+        outputs.add("Deadlock detected at time 2 involving...\n"
+                + "\tprocesses: 1 2\n\tresources: 2 1");
+
+        StringBuilder inputSb = new StringBuilder();
+        StringBuilder outputSb = new StringBuilder();
+
+        for (String input : inputs) {
+            inputSb.append(input).append("\n");
+        }
+        int i = 1;
+        for (String output : outputs) {
+            outputSb.append("Simulation ").append(i++).append("\n")
+                    .append(output).append("\n\n");
+        }
+
+        return new Object[][]{{null, inputSb.toString(), outputSb.toString()}};
     }
 
     @Test(dataProvider = "testCases")
